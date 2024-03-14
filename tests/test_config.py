faa85deb26d0d1e0143bec8e13b0241b91f5e8cc
Cam Ratchford <camratchford@gmail.com>
2024-03-14 10:54:13 -0600

    Added uninstall actions, refactored the config object to behave more consistently / be more modular, reconfigured tests to reflect new config, added uuid option to cli

--- tests/test_config.py
+++ tests/test_config.py
@@ -5,18 +5,26 @@ from msi_compiler.config import Config
 config_dict_paths_expanded = {
     "source_folder": str(Path(r".\fixtures\testpack").resolve()),
     "destination_folder": str(Path(r".\outputs\testdest").resolve()),
-    "custom_actions": [
+    "custom_install_actions": [
         {
-            "name": "TestAction1",
+            "name": "TestInstallAction1",
             "type": "powershell",
-            "target": r".\outputs\testdest\script.ps1",
+            "target": str(Path(r".\outputs\testdest\script.ps1").resolve()),
             "args": [1, 2]
         },
         {
-            "name": "TestAction2",
+            "name": "TestInstallAction2",
             "type": "executable",
             "target": r"C:\Windows\System32\msg.exe",
             "args": ["*", "Test Message"]
+        }
+    ],
+    "custom_uninstall_actions": [
+        {
+            "name": "TestUninstallAction1",
+            "type": "executable",
+            "target": r"C:\Windows\System32\msg.exe",
+            "args": ["*", "Uninstall Test Message"]
         }
     ],
 
@@ -25,11 +33,11 @@ config_dict_paths_expanded = {
     "package_version": "1.0.0",
     "company": "RatchfordConsulting",
     "manufacturer": "RatchfordManufacturing",
-    "properties": {
+    "msi_properties": {
         "ARPCONTACT": "camratchford@gmail.com",
-        "ARPPRODUCTICON": r".\outputs\testdest\package-box.ico",
+        "ARPPRODUCTICON": str(Path(r".\outputs\testdest\package-box.ico").resolve()),
         "ARPURLINFOABOUT": "https://support.ratchfordconsulting.com/",
-        "ARPREADME": r".\outputs\testdest\README.txt",
+        "ARPREADME": str(Path(r".\outputs\testdest\README.txt").resolve()),
         "UpgradeCode": "{39CFB886-7C1D-4469-A9C1-0C578E1C36D8}"
     },
     "environment_variables": [
@@ -54,27 +62,34 @@ config_dict_paths_expanded = {
 config_dict = {
     "source_folder": r".\fixtures\testpack",
     "destination_folder": r".\outputs\testdest",
-    "custom_actions": [
+    "custom_install_actions": [
         {
-            "name": "TestAction1",
+            "name": "TestInstallAction1",
             "type": "powershell",
             "target": str(Path(r".\outputs\testdest\script.ps1").resolve()),
             "args": [1, 2]
         },
         {
-            "name": "TestAction2",
+            "name": "TestInstallAction2",
             "type": "executable",
             "target": r"C:\Windows\System32\msg.exe",
             "args": ["*", "Test Message"]
         }
     ],
-
-    "msi_package_path": r".\outputs\{package_name}_{package_version}.msi",
+    "custom_uninstall_actions": [
+        {
+            "name": "TestUninstallAction1",
+            "type": "executable",
+            "target": r"C:\Windows\System32\msg.exe",
+            "args": ["*", "Uninstall Test Message"]
+        }
+    ],
+    "msi_package_path": str(Path(r".\outputs\{package_name}_{package_version}.msi").resolve()),
     "package_name": "TestPackage",
     "package_version": "1.0.0",
     "company": "RatchfordConsulting",
     "manufacturer": "RatchfordManufacturing",
-    "properties": {
+    "msi_properties": {
         "ARPCONTACT": "camratchford@gmail.com",
         "ARPPRODUCTICON": str(Path(r".\outputs\testdest\package-box.ico").resolve()),
         "ARPURLINFOABOUT": "https://support.ratchfordconsulting.com/",
@@ -105,12 +120,13 @@ def test_config_dict():
     config = Config(**config_dict_paths_expanded)
     assert config.source_folder == str(Path(r".\fixtures\testpack").resolve())
     assert config.destination_folder == str(Path(r".\outputs\testdest").resolve())
-    assert config.custom_actions == config_dict_paths_expanded.get('custom_actions')
-    assert config.msi_package_path == r".\outputs\TestPackage_1.0.0.msi"
+    assert config.custom_install_actions == config_dict_paths_expanded.get('custom_install_actions')
+    assert config.custom_uninstall_actions == config_dict_paths_expanded.get('custom_uninstall_actions')
+    assert config.msi_package_path == str(Path(r".\outputs\TestPackage_1.0.0.msi").resolve())
     assert config.package_version == "1.0.0"
     assert config.company == "RatchfordConsulting"
     assert config.manufacturer == "RatchfordManufacturing"
-    assert config.properties == config_dict_paths_expanded.get('properties')
+    assert config.msi_properties == config_dict_paths_expanded.get('msi_properties')
     assert config.package_name == "TestPackage"
 
 
@@ -119,10 +135,11 @@ def test_config_yaml():
     config = Config.from_file(config_path)
     assert config.source_folder == str(Path(r".\fixtures\testpack").resolve())
     assert config.destination_folder == str(Path(r".\outputs\testdest").resolve())
-    assert config.custom_actions == config_dict.get('custom_actions')
-    assert config.msi_package_path == r".\outputs\TestPackage_1.0.0.msi"
+    assert config.custom_install_actions == config_dict_paths_expanded.get('custom_install_actions')
+    assert config.custom_uninstall_actions == config_dict_paths_expanded.get('custom_uninstall_actions')
+    assert config.msi_package_path == str(Path(r".\outputs\TestPackage_1.0.0.msi").resolve())
     assert config.package_version == "1.0.0"
     assert config.company == "RatchfordConsulting"
     assert config.manufacturer == "RatchfordManufacturing"
-    assert config.properties == config_dict.get('properties')
+    assert config.msi_properties == config_dict_paths_expanded.get('msi_properties')
     assert config.package_name == "TestPackage"
